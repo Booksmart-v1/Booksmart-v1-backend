43e19994e2b17dc130b0aea38a16c4a1a7665e3a
Taarushthenoob <54764915+Taarushthenoob@users.noreply.github.com>
2022-10-12 21:38:34 +0530

    Changes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-let exp = require("express");
-const cors = require("cors");
+require('dotenv').config();
+let exp = require('express');
+const cors = require('cors');
 // const expressFileUpload = require("express-fileupload");
 
-let express = require("./configs/express");
-const mongoose = require("mongoose");
-const config = require("./configs/configs");
+let express = require('./configs/express');
+const mongoose = require('mongoose');
+const config = require('./configs/configs');
 // const socketio = require("socket.io");
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
+const multer = require('multer');
+// const { GridFsStorage } = require("multer-gridfs-storage");
 
 // socket configuration
-const WebSockets = require("./utils/WebSocket");
+const WebSockets = require('./utils/WebSocket');
 mongoose
   .connect(
-    "mongodb://user1:myyKK1HlvKedXxjN@booksmart-shard-00-00.zjufe.mongodb.net:27017,booksmart-shard-00-01.zjufe.mongodb.net:27017,booksmart-shard-00-02.zjufe.mongodb.net:27017/booksmart?replicaSet=atlas-g9eyff-shard-0&ssl=true&authSource=admin",
+    'mongodb://user1:myyKK1HlvKedXxjN@booksmart-shard-00-00.zjufe.mongodb.net:27017,booksmart-shard-00-01.zjufe.mongodb.net:27017,booksmart-shard-00-02.zjufe.mongodb.net:27017/booksmart?replicaSet=atlas-g9eyff-shard-0&ssl=true&authSource=admin',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -30,30 +30,30 @@ mongoose
 /** Create HTTP server. */
 /** Create socket connection */
 
-let bucket;
-mongoose.connection.on("connected", () => {
-  var db = mongoose.connections[0].db;
-  bucket = new mongoose.mongo.GridFSBucket(db, {
-    bucketName: "bookImg",
-  });
-  console.log(bucket);
-});
+// let bucket;
+// mongoose.connection.on("connected", () => {
+//   var db = mongoose.connections[0].db;
+//   bucket = new mongoose.mongo.GridFSBucket(db, {
+//     bucketName: "bookImg",
+//   });
+//   console.log(bucket);
+// });
 
-const httpsLocalhost = require("https-localhost")();
+const httpsLocalhost = require('https-localhost')();
 const certs = httpsLocalhost.getCerts();
 const app = express();
-const server = require("http").Server(certs, app);
-require("./app/modules/Portal/socket/socket")(server);
-global.io = require("socket.io")(server);
-global.io.on("connection", WebSockets.connection);
+const server = require('http').Server(certs, app);
+require('./app/modules/Portal/socket/socket')(server);
+global.io = require('socket.io')(server);
+global.io.on('connection', WebSockets.connection);
 
 app.use(cors());
 
-app.get("/", function (req, res, next) {
-  res.send("Welcome to Booksmart !");
+app.get('/', function (req, res, next) {
+  res.send('Welcome to Booksmart !');
 });
 
 server.listen(config.serverPort, () => {
-  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
   console.log(`Server running at http://localhost:${config.serverPort}`);
 });
--- app/Middlewares/upload.js
+++ app/Middlewares/upload.js
@@ -1,26 +1,26 @@
-const util = require('util');
-const multer = require('multer');
-const { GridFsStorage } = require('multer-gridfs-storage');
-const dbConfig = require('../../configs/configs');
+// const util = require('util');
+// const multer = require('multer');
+// const { GridFsStorage } = require('multer-gridfs-storage');
+// const dbConfig = require('../../configs/configs');
 
-var storage = new GridFsStorage({
-  url: 'mongodb://user1:myyKK1HlvKedXxjN@booksmart-shard-00-00.zjufe.mongodb.net:27017,booksmart-shard-00-01.zjufe.mongodb.net:27017,booksmart-shard-00-02.zjufe.mongodb.net:27017/booksmart?replicaSet=atlas-g9eyff-shard-0&ssl=true&authSource=admin',
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    const match = ['image/png', 'image/jpeg'];
+// var storage = new GridFsStorage({
+//   url: 'mongodb://user1:myyKK1HlvKedXxjN@booksmart-shard-00-00.zjufe.mongodb.net:27017,booksmart-shard-00-01.zjufe.mongodb.net:27017,booksmart-shard-00-02.zjufe.mongodb.net:27017/booksmart?replicaSet=atlas-g9eyff-shard-0&ssl=true&authSource=admin',
+//   options: { useNewUrlParser: true, useUnifiedTopology: true },
+//   file: (req, file) => {
+//     const match = ['image/png', 'image/jpeg'];
 
-    if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${Date.now()}-booksmart-${file.originalname}`;
-      return filename;
-    }
+//     if (match.indexOf(file.mimetype) === -1) {
+//       const filename = `${Date.now()}-booksmart-${file.originalname}`;
+//       return filename;
+//     }
 
-    return {
-      bucketName: dbConfig.imgBucket,
-      filename: `${Date.now()}-booksmart-${file.originalname}`,
-    };
-  },
-});
+//     return {
+//       bucketName: dbConfig.imgBucket,
+//       filename: `${Date.now()}-booksmart-${file.originalname}`,
+//     };
+//   },
+// });
 
-var uploadFiles = multer({ storage: storage }).single('file');
-var uploadFilesMiddleware = util.promisify(uploadFiles);
-module.exports = uploadFilesMiddleware;
+// var uploadFiles = multer({ storage: storage }).single('file');
+// var uploadFilesMiddleware = util.promisify(uploadFiles);
+// module.exports = uploadFilesMiddleware;
